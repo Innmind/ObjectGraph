a45088bf674975a5f6267a92d5344844e29e107e
Baptiste Langlade <baptouuuu@gmail.com>
2019-01-04 16:53:33 +0100

    give access to the file where the object class is defined

--- src/Node.php
+++ src/Node.php
@@ -7,6 +7,10 @@ use Innmind\ObjectGraph\{
     Node\ClassName,
     Node\Reference,
 };
+use Innmind\Url\{
+    UrlInterface,
+    Url,
+};
 use Innmind\Immutable\{
     Map,
     SetInterface,
@@ -21,8 +25,11 @@ final class Node
 
     public function __construct(object $object)
     {
+        $file = (new \ReflectionObject($object))->getFileName();
+
         $this->class = new ClassName($object);
         $this->reference = new Reference($object);
+        $this->location = Url::fromString('file://'.$file);
         $this->relations = Map::of('string', Relation::class);
     }
 
@@ -44,6 +51,11 @@ final class Node
     public function reference(): Reference
     {
         return $this->reference;
+    }
+
+    public function location(): UrlInterface
+    {
+        return $this->location;
     }
 
     /**
