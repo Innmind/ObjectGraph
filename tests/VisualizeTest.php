0ce7dc72133a7888bf1a27d1fdc88cefe5186b70
Baptiste Langlade <baptouuuu@gmail.com>
2019-10-27 11:52:28 +0100

    render dependents as a blue Mrecord

--- tests/VisualizeTest.php
+++ tests/VisualizeTest.php
@@ -134,4 +134,23 @@ class VisualizeTest extends TestCase
         $this->assertStringContainsString('#00ff00', (string) $dot);
         $this->assertSame(2, \substr_count((string) $dot, '#00ff00')); // root + edge
     }
+
+    public function testRenderDependent()
+    {
+        $graph = new Graph;
+        $leaf = new Foo;
+        $a = new Foo($leaf);
+        $b = new Foo($leaf);
+        $root = new Foo($a, $b);
+
+        $node = $graph($root);
+        $node->relations()->current()->node()->flagAsDependent();
+
+        $dot = (new Visualize)($node);
+
+        $this->assertInstanceOf(Readable::class, $dot);
+        $this->assertNotEmpty((string) $dot);
+        $this->assertStringContainsString('#00b6ff', (string) $dot);
+        $this->assertSame(1, \substr_count((string) $dot, '#00b6ff')); // a
+    }
 }
